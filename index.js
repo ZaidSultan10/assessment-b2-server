fc81b6d772fef0b05cc6634829ad737e1548f6f2
ZaidSultan10 <zaidsultan09@gmail.com>
2022-04-15 05:23:50 +0400

    typescript update

--- index.js
+++ index.js
@@ -17,16 +17,17 @@ wss.on('connection', function connection(ws) {
     let recievedData = JSON.parse(message)
 
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      //if you want to test the react part which has a button just do client !== ws in the condition below
+      if (client === ws && client.readyState === WebSocket.OPEN) {
         const getFunction = (parameter) => {
           let sendData = JSON.stringify(parameter)
           setTimeout(() => {
             client.send(sendData)
           },4000)
         }
-        if(recievedData.type === `Subscribe`){
+        if(recievedData.type === "Subscribe"){
           getFunction({type:"Subscribed"})
-        }else if(recievedData.type === `Unsubscribe`){
+        }else if(recievedData.type === "Unsubscribe"){
           getFunction({type:"Unsubscribed"})
         }else{
           getFunction({type:'Error'})
